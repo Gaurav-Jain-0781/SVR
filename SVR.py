2e210735d29eb6553801aa30e1024652b09040ee
Gaurav Jain <gauravjain0781@gmail.com>
2022-09-21 20:35:01 +0530

    Ploted the results of SVR model

--- SVR.py
+++ SVR.py
@@ -1,7 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 21 19:07:20 2022
+# SVR
 
-@author: Gaurav Jain
-"""
+# Data Preprocessing
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Importing Dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Fitting the SVR to the Dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel="rbf")
+regressor.fit(x, y)
+
+# Predicting the test set results
+y_predict = regressor.predict(x)
